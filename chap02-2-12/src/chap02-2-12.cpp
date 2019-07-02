11754b7bc9012052cdc5e87325c4bbf2a153f4ef
sato_hikaru <ddt.sato.h@gmail.com>
2019-07-02 20:44:54 +0900

    演習2-12 ネストを浅く変更 ifとelseの{の位置を統一

--- chap02-2-12/src/chap02-2-12.cpp
+++ chap02-2-12/src/chap02-2-12.cpp
@@ -37,27 +37,24 @@ int main()
 	//小さいほうの値を格納する変数と、大きい方の値を格納する変数を用意する。
 	int minimumValue, maxValue;
 
-	//同じ値かどうか判別する為にif文を利用し、異なる場合は大小比較を行う為elseを利用する。
-	if (firstValue == secondValue)
-	{
+	//一つ目の値と二つ目の値の大小を比較する為if文とelseを利用する。
+	if (firstValue < secondValue) {
+		//一つ目の値の方が小さい為、小さい方の値を格納する変数へ一つ目の値を格納する。
+		minimumValue = firstValue;
+		//二つ目の値の方が大きい為、大きい方の値を格納する変数へ二つ目の値を格納する。
+		maxValue = secondValue;
+	} else {
+		//二つ目の値の方が小さい為、小さい方の値を格納する変数へ二つ目の値を格納する。
+		minimumValue = secondValue;
+		//一つ目の値の方が大きい為、大きい方の値を格納する変数へ一つ目の値を格納する。
+		maxValue = firstValue;
+	}
+
+	//同じ値かどうか判別する為にif文を利用し、異なる場合はそれぞれの値の大小を表示させる為にelseを利用する。
+	if (firstValue == secondValue) {
 		//同じ値であることを確認する為に「二つの値は同じです。」と画面表示する。
 		cout << "二つの値は同じです。\n";
-
 	} else {
-		//一つ目の値と二つ目の値の大小を比較する為if文とelseを利用する。
-		if (firstValue < secondValue)
-		{
-			//一つ目の値の方が小さい為、小さい方の値を格納する変数へ一つ目の値を格納する。
-			minimumValue = firstValue;
-			//二つ目の値の方が大きい為、大きい方の値を格納する変数へ二つ目の値を格納する。
-			maxValue = secondValue;
-		} else {
-			//二つ目の値の方が小さい為、小さい方の値を格納する変数へ二つ目の値を格納する。
-			minimumValue = secondValue;
-			//一つ目の値の方が大きい為、大きい方の値を格納する変数へ一つ目の値を格納する。
-			maxValue = firstValue;
-		}
-
 		//どの値が最小値か確認する為に変数に代入した値を画面表示させる。
 		cout << "小さいほうの値は" << minimumValue << "です。\n";
 
